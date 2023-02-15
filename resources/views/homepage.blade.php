6ea0e3687c91a25c8c4c8dd36d9142aaa2908386
darkcheos <ericdellisse@gmail.com>
2023-02-15 16:34:38 +0100

    tweet

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('tweets') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="mt-8 text-2xl">
+                        tweets
+                    </div>
+
+                    <div class="mt-6 text-gray-500">
+                        <table class="table-auto w-full">
+                            <thead>
+                                <tr>
+                                    <th class="px-4 py-2">nom</th>
+                                    <th class="px-4 py-2">role</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($tweets as $tweet)
+                                    <tr>
+                                        <td class="border px-4 py-2">
+                                            {{ $tweet->id }}</td>
+                                        <td class="border px-4 py-2">
+                                            {{ $tweet->text }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
